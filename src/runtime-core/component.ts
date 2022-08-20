a22c0ab920b08e2c8fc2c40bcffe4ab7cb2b0269
caosiqi <caosiqi>
2022-08-20 17:43:13 +0800

    slots功能

---
+++ src/runtime-core/component.ts
@@ -0,0 +1,58 @@
+import { shallowReadonly } from "../reactivity/reactive";
+import { emit } from "./componentEmit";
+import { initProps } from "./componentProps";
+import { PublicInstanceProxyHandlers } from "./componentPublicInstance";
+import { initSlots } from "./componentSlots";
+
+export function createComponentInstance(vnode) {
+  const component = {
+    vnode,
+    type: vnode.type,
+    setupState: {},
+    props: {},
+    slots: {},
+    emit: () => {},
+  };
+
+  component.emit = emit.bind(null, component) as any;
+
+  return component;
+}
+
+export function setupComponent(instance) {
+  initProps(instance, instance.vnode.props);
+  initSlots(instance, instance.vnode.children);
+  setupStatefulComponent(instance);
+}
+
+function setupStatefulComponent(instance: any) {
+  const Component = instance.type;
+
+  instance.proxy = new Proxy({ _: instance }, PublicInstanceProxyHandlers);
+
+  const { setup } = Component;
+
+  if (setup) {
+    const setupResult = setup(shallowReadonly(instance.props), {
+      emit: instance.emit,
+    });
+
+    handleSetupResult(instance, setupResult);
+  }
+}
+
+function handleSetupResult(instance, setupResult: any) {
+  // function Object
+  // TODO function
+  if (typeof setupResult === "object") {
+    instance.setupState = setupResult;
+  }
+
+  finishComponentSetup(instance);
+}
+
+function finishComponentSetup(instance: any) {
+  const Component = instance.type;
+
+  instance.render = Component.render;
+}
